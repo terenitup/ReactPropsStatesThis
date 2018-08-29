02a5d73bd1ae27802af05f6f91e40083ca443744
terenitup <terentaniuchi@gmail.com>
2018-08-28 18:41:03 -0600

    added journal list component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import JournalList from './journals/journal_list';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Skeleton</h2>
+        <h1>React, Props, and State Deep Dive</h1>
+        <JournalList heading="List 1" />
+        <JournalList heading="List 2" />
       </div>
     );
   }
